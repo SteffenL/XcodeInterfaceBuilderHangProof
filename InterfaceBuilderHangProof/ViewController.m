a01d8d95a0e077380e5a5064e55b6a37eddeb968
Steffen André Langnes <steffenl.sw@gmail.com>
2016-05-14 13:54:14 +0200

    Initial Commit

---
+++ InterfaceBuilderHangProof/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  InterfaceBuilderHangProof
+//
+//  Created by Steffen Langnes on 14/05/16.
+//  Copyright © 2016 Steffen Langnes. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
